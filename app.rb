11a7bd691961d3ef76fb6b7fdb9fa16a6db24f23
suiyujin <suiyujin04@gmail.com>
2015-04-11 16:19:44 +0900

    extract addresses

--- app.rb
+++ app.rb
@@ -3,10 +3,16 @@ require 'json'
 require File.dirname(__FILE__) + '/google_geo_api'
 
 before do
-  content_type 'application/json'
+  content_type 'application/json', :charset => 'utf-8'
 end
 
 get '/' do
   latlng = "#{params['lat']},#{params['lng']}"
-  GoogleGeoApi.request(latlng)
+  response = GoogleGeoApi.request(latlng)
+  array_addresses = response['results'][0]['address_components'].map do |component|
+    {
+      long_name: component['long_name'],
+      type: component['types'][0]
+    }
+  end
 end
