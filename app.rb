b41eeef6e95878d1a59f66a7b237d3b1df8be53d
suiyujin <suiyujin04@gmail.com>
2015-04-12 00:32:33 +0900

    deploy for heroku

--- app.rb
+++ app.rb
@@ -3,20 +3,22 @@ require 'json'
 require File.dirname(__FILE__) + '/google_geo_api'
 require File.dirname(__FILE__) + '/google_news_api'
 
-before do
-  content_type 'application/json', :charset => 'utf-8'
-end
-
-get '/' do
-  latlng = "#{params['lat']},#{params['lng']}"
-  response = GoogleGeoApi.request(latlng)
-  array_addresses = response['results'][0]['address_components'].map do |component|
-    {
-      long_name: component['long_name'],
-      type: component['types'][0]
-    }
+class App < Sinatra::Base
+  before do
+    content_type 'application/json', :charset => 'utf-8'
   end
-  p array_addresses[0][:type]
-  address_index = array_addresses.find_index { |address| address[:type] == 'country' }
-  GoogleNewsApi.request(array_addresses[address_index][:long_name]).to_json
+
+  get '/' do
+    latlng = "#{params['lat']},#{params['lng']}"
+    response = GoogleGeoApi.request(latlng)
+    array_addresses = response['results'][0]['address_components'].map do |component|
+      {
+        long_name: component['long_name'],
+        type: component['types'][0]
+      }
+    end
+    p array_addresses[0][:type]
+    address_index = array_addresses.find_index { |address| address[:type] == 'country' }
+    GoogleNewsApi.request(array_addresses[address_index][:long_name]).to_json
+  end
 end
