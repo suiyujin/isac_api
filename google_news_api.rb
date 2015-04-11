dabcc3b519a346959317d3add93ac74da9c33b75
suiyujin <suiyujin04@gmail.com>
2015-04-12 03:13:03 +0900

    improve query of google news api

--- google_news_api.rb
+++ google_news_api.rb
@@ -2,6 +2,7 @@ require 'net/http'
 require 'uri'
 require 'json'
 require 'addressable/uri'
+require 'csv'
 
 class GoogleNewsApi
 
@@ -12,20 +13,25 @@ class GoogleNewsApi
       {
         v: '1.0',
         hl: 'ja',
+        ned: 'jp',
         rsz: '8',
-        q: "環境問題 #{address}"
+        q: make_query(address)
       }
     )
   end
 
   def self.make_query(address)
-
+    words = ''
+    CSV.foreach("#{File.dirname(__FILE__)}/words.csv", 'r') do |row|
+      words = row.join(' OR ')
+    end
+    "#{address} AND (#{words})"
   end
 
   def self.webapi(site, path, hash_params)
     params = hash_params.map { |key, value| "#{key}=#{value}" }.join('&')
+    p "#{site}#{path}#{params}"
     uri = Addressable::URI.parse(URI.escape("#{site}#{path}?#{params}"))
-    p uri
 
     https = Net::HTTP.new(uri.host, uri.port)
     res = https.start {
