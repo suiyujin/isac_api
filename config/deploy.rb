4dd0d56678427c92e0a05bb02d090a8fe60454d6
suiyujin <suiyujin04@gmail.com>
2015-04-25 04:08:04 +0900

    add default task :restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,15 @@ set :passenger_restart_command, 'rbenv sudo passenger-config restart-app'
 
 namespace :deploy do
 
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
